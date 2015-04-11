6a16099a9048076ccd430ed9e483c4064e8e0a8f
Нечаев Юрий <yesterdayy@ya.ru>
2015-04-11 03:08:01 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ if (!empty($_FILES)) {
  $tempFile = $_FILES['file']['tmp_name'];
  $filenameFileLocal = iconv("UTF-8","WINDOWS-1251",$_FILES['file']['name']); 
  // using DIRECTORY_SEPARATOR constant is a good practice, it makes your code portable.
- $targetPath = $_SERVER['DOCUMENT_ROOT'].'/backup/';
+ $targetPath = $_SERVER['DOCUMENT_ROOT'].'/directory/';
  // Adding timestamp with image's name so that files with same name can be uploaded easily.
  $mainFile = $targetPath . $filenameFileLocal;
  move_uploaded_file($tempFile,$mainFile);
